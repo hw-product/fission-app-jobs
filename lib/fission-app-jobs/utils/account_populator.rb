da66b20c61b72a275793fdfa362d56cfe178509b
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-07 07:13:10 -0800

    Update name to `account_populator` and disable full remote repository retrieval

--- lib/fission-app-jobs/utils/account_populator.rb
+++ lib/fission-app-jobs/utils/account_populator.rb
@@ -6,8 +6,8 @@ module Fission
     module Jobs
       class Utils
 
-        # Populates repository data using given user credentials
-        class RepositoryPopulator < Utils
+        # Populates account data using given user credentials
+        class AccountPopulator < Utils
 
           # user:: Fission::Data::User instance or user ID
           # args:: Optionally specify specific sources (:github)
@@ -31,52 +31,70 @@ module Fission
               ident.provider.to_sym == :github
             end.credentials['token']
             client = Octokit::Client.new(:access_token => token)
-            # Fetch all repos the user owns
-            fetch_all(client.user, :repos).each do |repo|
-              add_github_repository(repo, user)
-            end
-            # Fetch all repos the user has access to
-            fetch_all(client.user, :organizations).each do |org|
-              fetch_all(org, :repos).each do |repo|
-                add_github_repository(repo, user)
+            accounts = {}
+            client.user_teams(:per_page => 100).each do |team|
+              account = github_org_account(team.organization)
+              accounts[account.id] ||= []
+              if(team.name == 'Owners')
+                fetch_all(team.organization, :repos).each do |repo|
+                  add_github_repository(repo, account)
+                end
+                accounts[account.id] << :owner
+              else
+                accounts[account.id] << :member
               end
             end
+            accounts.each do |account_id, addition|
+              account = Account[account_id]
+              if(addition.include?(:owner))
+                account.add_owners(user)
+              else
+                account.add_members(user)
+              end
+              account.save
+            end
+            # Fetch all repos the user owns
+            # NOTE: This is disabled, likely to be removed.
+            # account = user.base_account
+            # fetch_all(client.user, :repos).each do |repo|
+              # add_github_repository(repo, account)
+            # end
+            true
           end
 
           # repo:: Octokit repo instance
-          # user:: Fission::Data::User instance
+          # account:: Fission::Data::Account instance
           # Add repo to fission data
-          def add_github_repository(repo, user)
+          def add_github_repository(repo, account)
             repository = Fission::Data::Repository.find_by_url(repo.rels[:git].href) || Fission::Data::Repository.new
             repository.name = repo.name
             repository.url = repo.rels[:git].href
             repository.clone_url = repo.rels[:clone].href
             repository.private = repo.private
-            repository.owner = get_repo_owner(repo, user)
+            repository.owner = account
             unless(repository.save)
               raise "Failed to save repository! #{repository.errors.join(', ')}"
             end
             repository
           end
 
-          private
-
-          # repo:: Octokit repo instance
-          # user:: Fission::Data::User instance
-          # Return Fission::Data::Account owner for given repository.
-          # If account does not exist, one will be created.
-          def get_repo_owner(repo, user)
-            owner_name = repo.owner.login
-            account = Fission::Data::Account.find_by_name(owner_name) || Fission::Data::Account.new(:name => owner_name)
-            account.add_members(user)
-            if(repo.owner.type.to_s.downcase == 'organization')
-              # auto add members!
-            end
-            unless(account.save)
-              raise "Failed to save account! #{account.errors.join(', ')}"
+          # org:: Octokit org resource
+          # Create new account based on org information or return existing
+          def github_org_account(org)
+            account = Fission::Data::Account.lookup(org.login, :github)
+            unless(account)
+              account = Fission::Data::Account.new
+              account.name = org.login
+              account.source = :github
+              account.email = org.email
+              unless(account.save)
+                raise "Failed to save account! - #{account.errors.join(', ')}"
+              end
             end
             account
           end
+
+          private
 
           # base:: Base resource
           # key:: Key to retrieve data
