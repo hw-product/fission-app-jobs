bbbfe30129b64dc52072aad7cd44d0f3883079d9
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-27 13:29:27 -0800

    Remove fetch all method (no longer fetching the world)

--- lib/fission-app-jobs/utils/account_populator.rb
+++ lib/fission-app-jobs/utils/account_populator.rb
@@ -94,26 +94,6 @@ module Fission
             account
           end
 
-          private
-
-          # base:: Base resource
-          # key:: Key to retrieve data
-          # Follows all pages and returns data.
-          def fetch_all(base, key)
-            result = []
-            resource = base.rels[key].get
-            result += [resource.data].flatten(1).compact
-            while(resource.rels[:next])
-              resource = resource.rels[:next].get
-              result += [resource.data].flatten(1).compact
-            end
-            if(block_given?)
-              yield result
-            else
-              result
-            end
-          end
-
         end
 
       end
