36928ab0da8fac0b9a0f20835ee0dc6d0553232c
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-19 14:07:59 -0800

    Add dynamic route inserter method helper

--- lib/fission-app-jobs/engine.rb
+++ lib/fission-app-jobs/engine.rb
@@ -1,24 +1,18 @@
-require 'fission-app-multiuser'
-
 module FissionApp
   module Jobs
     class Engine < ::Rails::Engine
-
-      config.to_prepare do |config|
-        require 'fission-data/init'
-        product = Fission::Data::Models::Product.find_or_create(:name => 'Jobs')
-        feature = Fission::Data::Models::ProductFeature.find_or_create(
-          :name => 'jobs_information',
-          :product_id => product.id
-        )
-        unless(feature.permissions_dataset.where(:name => 'jobs_info_access').count > 0)
-          feature.add_permission(
-            :name => 'jobs_info_access',
-            :pattern => '/jobs(/show.*)'
-          )
-        end
-      end
-
     end
+
+    # Provide proc for dynamic route injections.
+    #
+    # @return [Proc] block for injecting routes
+    def self.jobs_routes
+      Proc.new do |namespace|
+        get "#{namespace}/jobs(/:payload_filter/:payload_value)", :defaults => {:namespace => namespace}, :to => 'jobs#all'
+        get "#{namespace}/:account_id/jobs(/:payload_filter/:payload_value)", :defaults => {:namespace => namespace}, :to => 'jobs#list'
+        get "#{namespace}/job/:job_id", :defaults => {:namespace => namespace}, :to => 'jobs#details'
+      end
+    end
+
   end
 end
