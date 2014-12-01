81b161d8a3a9a1da313604cb16bc1c39fea7fb95
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-01 14:27:41 -0800

    Use router action to pull job subset for summary

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,14 +9,23 @@ class JobsController < ApplicationController
   end
 
   before_action do
-    @valid_jobs = Job.dataset.join_table(:left, :jobs___j2){ |j2, j| ({Sequel.qualify(j, :message_id) => Sequel.qualify(j2, :message_id)}) & (Sequel.qualify(j, :created_at) < Sequel.qualify(j2, :created_at))}.where(:j2__id => nil).select(:jobs__id)
     @product = Product.find_by_internal_name(params[:namespace])
+    valid_jobs = Job.dataset.join_table(:left, :jobs___j2){ |j2, j| ({Sequel.qualify(j, :message_id) => Sequel.qualify(j2, :message_id)}) & (Sequel.qualify(j, :created_at) < Sequel.qualify(j2, :created_at))}.where(:j2__id => nil).select(:jobs__id)
+    @valid_jobs = Job.dataset_with(
+      :scalars => {
+        :route => ['data', 'router', 'action']
+      }
+    ).where(
+      :route => params[:namespace].to_s,
+      :account_id => @accounts.map(&:id),
+      :id => valid_jobs
+    ).select(:id)
     @jobs_complete = Job.dataset_with_complete.where(
       :account_id => @accounts.map(&:id)
-    ).where('? = ANY(complete)', params[:namespace].to_s)
+    ).where('? = ANY(complete)', 'package_builder') #params[:namespace].to_s)
     @jobs_inprogress = Job.dataset_with_router.where(
       :account_id => @accounts.map(&:id)
-    ).where('? = ANY(router)', params[:namespace].to_s)
+    ).where('? = ANY(router)', 'package_builder') #params[:namespace].to_s)
     @jobs_error = Job.dataset_with(
       :scalars => {
         :error => ['error', 'callback']
@@ -55,68 +64,8 @@ class JobsController < ApplicationController
       end
       format.html do
         complete_jobs = @jobs_complete.select(:id).all.map(&:id) + @jobs_inprogress.select(:id).all.map(&:id) + @jobs_error.select(:id).all.map(&:id)
-        @jobs = Job.where(:id => @valid_jobs).where(:id => complete_jobs).all
+        @jobs = Job.where(:id => @valid_jobs).order(:created_at.desc)
         @progress = (@jobs.map(&:percent_complete).sum.to_f / @jobs.count)
-        @data = Hash[
-          @accounts.map do |acct|
-            [
-              acct,
-              Smash.new(
-                :in_progress => @jobs_inprogress.where(:id => @valid_jobs, :account_id => acct.id).all,
-                :completed => @jobs_complete.where(:id => @valid_jobs, :account_id => acct.id).all
-              )
-            ]
-          end
-        ]
-      end
-    end
-  end
-
-  def list
-  end
-
-  def details
-  end
-
-
-
-  def index
-    respond_to do |format|
-      format.js do
-        flash[:error] = 'Unsupported request!'
-        if(@account)
-          javascript_redirect_to account_jobs_path(@account)
-        else
-          javascript_redirect_to jobs_path
-        end
-      end
-      format.html do
-        @jobs = @jobs.order(:created_at.desc).paginate(page, per_page)
-        enable_pagination_on(@jobs)
-      end
-    end
-  end
-
-  def show
-    respond_to do |format|
-      format.js do
-        flash[:error] = 'Unsupported request!'
-        if(@account)
-          javascript_redirect_to account_jobs_path(@account)
-        else
-          javascript_redirect_to jobs_path
-        end
-      end
-      format.html do
-        @job = @jobs.where(:id => params[:id]).first
-        @history = @jobs.where(:message_id => @job.message_id).order(:created_at.desc)
-        unless(@job)
-          if(@account)
-            javascript_redirect_to account_jobs_path(@account)
-          else
-            javascript_redirect_to jobs_path
-          end
-        end
       end
     end
   end
