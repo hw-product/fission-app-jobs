5a976c09978ebb5602329aaffab60fa45fcf980f
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-03 12:44:27 -0800

    Remove account reference from routes

--- lib/fission-app-jobs/engine.rb
+++ lib/fission-app-jobs/engine.rb
@@ -9,7 +9,7 @@ module FissionApp
     def self.jobs_routes
       Proc.new do |namespace|
         get "#{namespace}/jobs(/:payload_filter/:payload_value)", :defaults => {:namespace => namespace}, :to => 'jobs#all'
-        get "#{namespace}/:account_id/jobs(/:payload_filter/:payload_value)", :defaults => {:namespace => namespace}, :to => 'jobs#list'
+        get "#{namespace}/jobs(/:payload_filter/:payload_value)", :defaults => {:namespace => namespace}, :to => 'jobs#list'
         get "#{namespace}/job/:job_id", :defaults => {:namespace => namespace}, :to => 'jobs#details'
       end
     end
