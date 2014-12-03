66495f3e697652ecb7f4e853851b1c9993203d8f
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-03 13:06:31 -0800

    Scrub out query building helpers from before action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,61 +2,17 @@ require 'base64'
 
 class JobsController < ApplicationController
 
-  prepend_before_action do
-    if(params[:job_id])
-      params[:account_id] = Job.find(params[:job_id]).try(:account_id)
-    end
-  end
-
   before_action do
-    unless(@product)
-      raise "Must define product for job listing. Full list currently disabled"
-    end
-    valid_jobs = Job.dataset.join_table(:left, :jobs___j2){ |j2, j| ({Sequel.qualify(j, :message_id) => Sequel.qualify(j2, :message_id)}) & (Sequel.qualify(j, :created_at) < Sequel.qualify(j2, :created_at))}.where(:j2__id => nil).select(:jobs__id)
     @valid_jobs = Job.dataset_with(
       :scalars => {
-        :route => ['data', 'router', 'action']
+        :route => ['data', 'router', 'action'],
+        :status => ['status']
       }
     ).where(
       :route => params[:namespace].to_s,
-      :account_id => @account.id,
-      :id => valid_jobs
-    ).select(:id)
-    @jobs_complete = Job.dataset_with_complete.where(
-      :account_id => @account.id
-    ).where('? = ANY(complete)', 'package_builder') #params[:namespace].to_s)
-    @jobs_inprogress = Job.dataset_with_router.where(
-      :account_id => @account.id
-    ).where('? = ANY(router)', 'package_builder') #params[:namespace].to_s)
-    @jobs_error = Job.dataset_with(
-      :scalars => {
-        :error => ['error', 'callback']
-      }
-    ).where("error like ?", "#{params[:namespace]}:%")
-    if(params[:payload_filter])
-      location = MultiJson.load(Base64.urlsafe_decode64(params[:payload_filter]))
-      value = Base64.urlsafe_decode64(params[:payload_value])
-      @jobs_inprogress = Job.dataset_with(
-        :scalars => {
-          :custom_search => location
-        }
-      ).where(:id => @jobs_inprogress.select(:id)).
-        where(:custom_search => value)
-      @jobs_complete = Job.dataset_with(
-        :scalars => {
-          :custom_search => location
-        }
-      ).where(:id => @jobs_complete.select(:id)).
-        where(:custom_search => value)
-    end
+      :account_id => current_user.run_state.current_account.id
+    )
   end
-
-  ## Need success and error counts
-  # -> query for error payloads
-  # -> subtract/remove from completed depending on view
-  # -> need to add group by and squash so we showing jobs and not
-  #counting history
-
 
   def all
     respond_to do |format|
@@ -65,9 +21,9 @@ class JobsController < ApplicationController
         javascript_redirect_to dashboard_path
       end
       format.html do
-        complete_jobs = @jobs_complete.select(:id).all.map(&:id) + @jobs_inprogress.select(:id).all.map(&:id) + @jobs_error.select(:id).all.map(&:id)
-        @jobs = Job.where(:id => @valid_jobs).order(:created_at.desc)
-        @progress = (@jobs.map(&:percent_complete).sum.to_f / @jobs.count)
+        @jobs = @valid_jobs.order(:created_at.desc).paginate(page, per_page)
+        @progress = (@jobs.all.map(&:percent_complete).sum.to_f / @jobs.count)
+        enable_pagination_on(@jobs)
       end
     end
   end
