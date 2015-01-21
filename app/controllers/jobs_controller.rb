a02fdef4339c8e4eea37fced6228393ff7d8280d
Chris Roberts <chrisroberts.code@gmail.com>
2015-01-21 05:58:45 -0800

    Link out from job listing. Provide default job details (this route needs overrides within specific engines)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -23,7 +23,25 @@ class JobsController < ApplicationController
       format.html do
         @jobs = @valid_jobs.order(:created_at.desc).paginate(page, per_page)
         @progress = (@jobs.all.map(&:percent_complete).sum.to_f / @jobs.count)
+        @namespace = @product.internal_name
         enable_pagination_on(@jobs)
+      end
+    end
+  end
+
+  def details
+    respond_to do |format|
+      format.js do
+        flash[:error] = 'Unsupported request!'
+        javascript_redirect_to dashboard_path
+      end
+      format.html do
+        @job = @valid_jobs.where(:message_id => params[:job_id]).first
+        @history = []
+        unless(@job)
+          flash[:error] = "Failed to locate requested job (ID: #{params[:job_id]})"
+          redirect_to dashboard_path
+        end
       end
     end
   end
